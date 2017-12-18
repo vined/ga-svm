21d2c8f40d9f7f56ae8712f626e1376aca38bcfd
Edvinas Naraveckas <e.naraveckas@gmail.com>
2017-12-18 09:48:02 +0200

    Scoring algorithm

--- src/ga-svm.hs
+++ src/ga-svm.hs
@@ -7,7 +7,7 @@ module Main where
 import GHC.Float (float2Double)
 import System.IO (IOMode(ReadMode), hGetContents, withFile)
 import System.Environment (getArgs)
-import Data.List (drop, foldl, head, last, length, map, take, zip)
+import Data.List (drop, foldl, filter, head, last, length, map, take, zip, zipWith)
 import System.Random (StdGen(..), mkStdGen, randomR, randoms)
 import GA (Entity(..), GAConfig(..), ScoredEntity(..), evolveVerbose)
 
@@ -18,6 +18,8 @@ import GA (Entity(..), GAConfig(..), ScoredEntity(..), evolveVerbose)
 
 
 -- Data types --
+
+data Score = TP | TN | FP | FN deriving (Eq, Show)
 
 type Cls = Int
 data SV = SV {clss :: Cls, values:: [Double]} deriving (Show)
@@ -175,6 +177,70 @@ mutateRule rule stepRate initialRanges seed =
     in Rule {cls=(cls rule), features=newFeatures}
 
 
+-- Scoring Utils --
+
+divIntsToDbl :: Int -> Int -> Double
+divIntsToDbl a b = (fromIntegral a) / (fromIntegral b)
+
+
+isInRange :: Feature -> Double -> Bool
+isInRange feature val =
+    let r = (range feature)
+        o = (on feature)
+    in if o
+        then val >= (from r) && val <= (to r)
+        else True
+
+
+evaluateFeatures :: [Feature] -> [Double] -> [Bool]
+evaluateFeatures fts vals =
+    zipWith isInRange fts vals
+
+
+evaluateRule :: Rule -> SV -> Score
+evaluateRule rule sv =
+    let svClass = (clss sv)
+        ruleClass = (cls rule)
+        fts = (features rule)
+        vals = (values sv)
+        evals = evaluateFeatures fts vals
+        allTrue = foldl (&&) True evals
+    in
+        if ruleClass == 1 && svClass == 1
+            then if allTrue
+                then TP
+                else FN
+            else if ruleClass == 0 && svClass == 0
+                then if allTrue
+                    then TN
+                    else FP
+                else if ruleClass == 1 && not (ruleClass == svClass)
+                    then if allTrue
+                        then FP
+                        else TN
+                    else if allTrue -- ruleClass == 0 && not ruleClass == svClass
+                        then FN
+                        else TP -- TN or TP?
+
+
+countScore :: Score -> [Score] -> Int
+countScore score scores = length $ filter (==score) scores
+
+
+scoreEntity :: [SV] -> Rule -> Double
+scoreEntity svs rule =
+    let scores = map (\sv -> evaluateRule rule sv) svs
+        tpCnt = countScore TP scores
+        tnCnt = countScore TN scores
+        fpCnt = countScore FP scores
+        fnCnt = countScore FN scores
+        allCnt = length scores
+        sensitivity = divIntsToDbl tpCnt (tpCnt + fnCnt)
+        specificity = divIntsToDbl tnCnt (tnCnt + fpCnt)
+    in
+        1 - (sqrt (sensitivity * specificity))
+
+
 -- GA --
 
 -- Entity entityType populationType dataSetType poolType monad
@@ -195,12 +261,9 @@ instance Entity Rule Double [SV] [Range] IO where
     mutation initialRanges stepRate seed entity =
         return $ Just $ mutateRule entity stepRate initialRanges seed
 
-    -- score entity (lower is better) sqrt(sensitivity + specificity)
-    -- sensitivity TP / (TP + FN)
-    -- specificity TN / (TN + FP)
-    -- but also active
+    -- score entity (lower is better)
     score' dataSet entity =
-        Just 0
+        Just $ scoreEntity dataSet entity
 
 
 main :: IO ()
@@ -212,6 +275,11 @@ main = do
         let svs = readDataSetFileLines $ lines dataSetContents
             initialRanges = getInitialRanges svs
             rangesForPrinting = rangesToPrettyStrings initialRanges
+
+            testRule = createRuleFromRanges 1 initialRanges [True, False, False, False, False, False, False, False, False]
+            firstSv = head svs
+            firstEval = map (\sv -> evaluateRule testRule sv) svs
+
             seed = 0
             g = mkStdGen seed
             cfg = GAConfig
@@ -219,22 +287,46 @@ main = do
                     20 -- archive size (best entities to keep track of)
                     200 -- maximum number of generations (use 1000)
                     0.2 -- crossover rate (% of entities by crossover) (from 0.2 to 0.6)
-                    0.05 -- mutation rate (% of entities by mutation) (from 0.01 to 0.1)
+                    0.5 -- mutation rate (% of entities by mutation) (from 0.01 to 0.1)
                     0.0 -- parameter passed to crossover (not used)
-                    0.001 -- parameter passed to mutation (attribute mutation max step size coef)
+                    0.8-- parameter passed to mutation (attribute mutation max step size rate)
                     False -- whether or not to use check-pointing
                     True -- don't re-score archive in each generation
 
         putStrLn "Min max values"
         sequence_ $ map putStrLn rangesForPrinting
 
-        putStrLn $ show $ getMaxStep 0.001 $ head initialRanges
-
         -- Do the evolution
         es <- evolveVerbose g cfg initialRanges svs
-        putStrLn $ "Best entity: " ++ (ruleToString $ snd $ head es)
-        putStrLn "-- All entities --"
+--         putStrLn $ "Best entity: " ++ (ruleToString $ snd $ head es)
+        putStrLn "-- First 10 entities --"
         sequence_ $ map putStrLn $ map (\(s, r) -> "Score: " ++ (show s) ++ ", " ++ (ruleToString r)) (take 10 es)
+        putStrLn "-- Last 10 entities --"
+        sequence_ $ map putStrLn $ map (\(s, r) -> "Score: " ++ (show s) ++ ", " ++ (ruleToString r)) (take 10 (reverse es))
+
+
+        putStrLn "-- Testing --"
+        putStrLn $ ruleToString testRule
+
+        putStrLn $ show $ isInRange (head (features testRule)) (head (values firstSv))
+        putStrLn $ show $ isInRange ((features testRule) !! 1) ((values firstSv) !! 1)
+        putStrLn "-- evaluateFeatures --"
+        sequence_ $ map putStrLn $ map show (evaluateFeatures (features testRule) (values firstSv))
+        putStrLn "-- evaluateRule --"
+        putStrLn $ show $ evaluateRule testRule (head svs)
+        sequence_ $ map putStr $ map (\ev -> (show ev) ++ ",") firstEval
+
+        putStrLn "\n"
+
+        putStrLn $ "Result size: " ++ (show $ length es)
+
+        putStrLn "-- First entity --"
+        putStrLn ("Score: " ++ (show $ fst $ head es) ++ ", " ++ (ruleToString $ snd $ head es))
+        putStrLn "-- Last entity --"
+        putStrLn ("Score: " ++ (show $ fst $ last es) ++ ", " ++ (ruleToString $ snd $ last es))
+
+        putStrLn $ show $ scoreEntity svs testRule
+
 
         putStrLn "Done.")
 
